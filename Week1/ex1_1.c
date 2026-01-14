94e582e41c051c5f234fac2b02b4a634bfec457e
eliel <eliel.j.levine@gmail.com>
2026-01-14 17:46:38 +0200

    Works

--- Week1/ex1_1.c
+++ Week1/ex1_1.c
@@ -3,22 +3,63 @@
 float money(void);
 float bus_cost(void);
 float taxi_cost(void);
+int choice(void);
 
 int main(void) {
     float your_money = 0;
+    float buscost = 0;
+    float taxicost = 0;
+
+    buscost= bus_cost();
+    taxicost = taxi_cost();
     your_money = money();
 
-    float buscost = 0;
-    buscost= bus_cost();
 
-    float taxicost = 0;
-    taxicost = taxi_cost();
-    printf("You have: $%.2f\n" , your_money);
-    printf("A bus ticket costs: $%.2f\n"  , buscost);
-    printf("A taxi costs: $%.2f\n" , taxicost);
+    while (your_money >= buscost || your_money >= taxicost) {
+        int select = 0;
+        printf("Do you want to travel by \n 1) Bus ($%.2f)\n 2) Taxi ($%.2f)\n",buscost,taxicost);
+        select = choice();
+        switch (select) {
+            case 1:
+                if (your_money >= buscost) {
+                    printf("You chose bus.\n");
+                    your_money -= buscost;
+                    printf("You have: $%.2f left. \n" , your_money);
+                    break;
+                }
+                if (your_money <= buscost) {
+                    printf("You chose bus.\n");
+                    printf("You don't have enough money for that. \n");
+                    printf("You have: $%.2f left. \n" , your_money);
+                    break;
+                }
+            case 2:
+                if (your_money >= taxicost) {
+                    printf("You chose taxi.\n");
+                    your_money -= taxicost;
+                    printf("You have: $%.2f left. \n" , your_money);
+                    break;
+                }
+                if (your_money <= taxicost) {
+                    printf("You chose taxi.\n");
+                    printf("You don't have enough money for that. \n");
+                    printf("You have: $%.2f left. \n" , your_money);
+                    break;
+                }
+            default:
+                printf("Please enter a valid number\n");
+                break;
+        }
+    }
+    printf("You don't have enough money, take a walk. Bye\n");
     return 0;
 }
-
+int choice() {
+    int var = 0;
+    printf("Enter your selection: ");
+    scanf("%d", &var);
+    return var;
+}
 float bus_cost() {
     float bus;
     printf("How much does a bus ticket cost?: ");
