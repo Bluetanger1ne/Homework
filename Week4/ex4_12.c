6cbe51942259086ca4f1a6b99ab7e577f6a17b9c
eliel <eliel.j.levine@gmail.com>
2026-02-06 12:16:25 +0200

    Initial.

---
+++ Week4/ex4_12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int count_words(const char* str, const char *word);
+
+int main() {
+
+
+
+    return 0;
+}
+
+
+int count_words(const char* str, const char *word) {
+
+
+}
