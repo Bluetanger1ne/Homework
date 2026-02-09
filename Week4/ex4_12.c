03a7bcf33003d873d51cc2fccc3d730d562979d0
eliel <eliel.j.levine@gmail.com>
2026-02-09 12:20:47 +0200

    Added more printing specifiers, and comments.

--- Week4/ex4_12.c
+++ Week4/ex4_12.c
@@ -11,24 +11,24 @@ int main() {
     int running = 1;
 
     do {
-        printf("PLease enter a string: \n");
+        printf("\nPlease enter a string:");
         fgets(sentence,MAX_SIZE,stdin);
         if (sentence[strlen(sentence) - 1] == '\n') {
             // Removing the new line
             sentence[strlen(sentence) - 1] = '\0'; // Replacing it with \0
         }
-        printf("Please enter a word you wish to search for: \n");
+        printf("\nPlease enter a word you wish to search for: \n");
         fgets(word,MAX_SIZE,stdin);
         if (word[strlen(word) - 1] == '\n') {
             // Removing the new line
             word[strlen(word) - 1] = '\0'; // Replacing it with \0
         }
         if (strcmp(word, "stop") == 0 || strcmp(word, "Stop") == 0 || strcmp(word, "STOP") == 0)  {
-            running = 0;
+            running = 0; // Checking if the user enters the word 'stop' in (almost) any iteration.
         }
         if (running != 0) {
             int total_amount = count_words(sentence,word);
-            printf("%d\n",total_amount);
+            printf("That word appeared %d time(s) in the sentence.\n",total_amount);
         }
 
     } while (running != 0);
@@ -38,10 +38,10 @@ int main() {
 
 int count_words(const char* str, const char *word) {
     int count = 0;
-    const char *ptr = str;
+    const char *ptr = str; // Using pointer arithmetic to keep track of each word found within the sentence.
 
     while ((ptr = strstr(ptr, word))) {
-        ptr += strlen(word);
+        ptr += strlen(word); // If the word is found, jump over the section where it was found, and add one to the total count.
         count++;
     }
 
